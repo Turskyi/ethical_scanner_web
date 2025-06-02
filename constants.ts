13e9fdc502ab21cbec90b9627cd417a2e607d9df
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2025-06-02 19:56:25 -0400

    :kissing: Refactor Support page: Use SUPPORT_EMAIL constant for contact email and standardize string quotes

--- constants.ts
+++ constants.ts
@@ -5,3 +5,4 @@ export const APP_STORE =
   'https://apps.apple.com/ca/app/ethical-scanner/id6743681663';
 export const FLUTTER_WEB_URL = 'https://ethical-scanner.web.app';
 export const FLUTTER_WEB_URL_SCAN = 'https://ethical-scanner.web.app/#/scan';
+export const SUPPORT_EMAIL = 'support@ethical-scanner.com';
--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { SUPPORT_EMAIL } from '../../../constants';
 
 export default function Support() {
   return (
     <main
       className="flex min-h-screen flex-col items-center justify-center p-12"
-      style={{ background: "linear-gradient(to bottom, #000C40, #F0F2F0)" }}
+      style={{ background: 'linear-gradient(to bottom, #000C40, #F0F2F0)' }}
     >
       <h1 className="mb-6 text-4xl font-bold text-white">Support</h1>
       <p className="mb-4 max-w-2xl text-center text-lg text-white">
@@ -12,16 +13,13 @@ export default function Support() {
         Scanner app, please don’t hesitate to contact us.
       </p>
       <p className="mb-4 text-lg text-white">
-        📧 Email us at:{" "}
-        <a
-          href="mailto:support@turskyi.com"
-          className="text-blue-200 underline"
-        >
-          support@turskyi.com
+        📧 Email us at:{' '}
+        <a href={`mailto:${SUPPORT_EMAIL}`} className="text-blue-200 underline">
+          {SUPPORT_EMAIL}
         </a>
       </p>
       <p className="mb-4 text-lg text-white">
-        💬 Join our support group on{" "}
+        💬 Join our support group on{' '}
         <a
           href="https://t.me/+B5gN1BLsVPo3M2My"
           target="_blank"
@@ -29,12 +27,12 @@ export default function Support() {
           className="text-blue-200 underline"
         >
           Telegram
-        </a>{" "}
+        </a>{' '}
         to ask questions or chat with other users.
       </p>
-      
+
       <p className="mb-4 text-lg text-white">
-        🌐 You can also visit the{" "}
+        🌐 You can also visit the{' '}
         <a
           href="https://turskyi.com/#/support"
           target="_blank"
@@ -42,7 +40,7 @@ export default function Support() {
           className="text-blue-200 underline"
         >
           support page on turskyi.com
-        </a>{" "}
+        </a>{' '}
         for more help.
       </p>
       <p className="text-md mb-8 max-w-xl text-center text-white">
